f47b53fa09a358df106e00d6e96612a3a83634e2
garudasec <228494846+garudasec@users.noreply.github.com>
2026-07-20 10:46:17 +0530

    frontend services url update

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,6 +1,6 @@
 const backendApiUrl = import.meta.env.VITE_BACKEND_URL;
 
-export const createuserapi = `${backendApiUrl}/createuser`;
-export const getuserapi = `${backendApiUrl}/getuser`;
-export const updateuserapi = `${backendApiUrl}/updateuser`;
-export const deleteuserapi = `${backendApiUrl}/deleteuser`;
+export const createuserapi = `${backendApiUrl}/createUser`;
+export const getuserapi = `${backendApiUrl}/getUser`;
+export const updateuserapi = `${backendApiUrl}/updateUser`;
+export const deleteuserapi = `${backendApiUrl}/deleteUser`;
